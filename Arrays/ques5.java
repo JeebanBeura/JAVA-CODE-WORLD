6a81032676e1228b1a06b04b023f45ed03081ff3
jeebanCodes <139693626+JeebanBeura@users.noreply.github.com>
2024-02-09 15:49:06 +0530

    Update ques5.java

--- Arrays/ques5.java
+++ Arrays/ques5.java
@@ -5,6 +5,34 @@ package Arrays;
 import java.util.Scanner;
 public class ques5 {
     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+        
+        int array[] = new int[size];
+        
+        System.out.print("Enter array elements : ");  
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int temp;
+        
+        for (int i = 0; i < array.length; i++) {
+            while(1 <= array[i] && array[i] <= size && array[i] != array[array[i] - 1]){
+                temp = array[i];
+                array[i] = array[temp - 1];
+                array[temp - 1] = temp;
+            }
+             
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (array[i] != i + 1){ 
+                System.out.print("Missing integer is : " + (i + 1));
+                break;
+            }
+        }
     }
 }
