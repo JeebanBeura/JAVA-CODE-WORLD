2c44f42829cfcf4c2717c08b8ff3dfe91684ab95
jeebanCodes <139693626+JeebanBeura@users.noreply.github.com>
2024-02-09 15:46:47 +0530

    Update ques7.java

--- Arrays/ques7.java
+++ Arrays/ques7.java
@@ -2,14 +2,38 @@
 Q-7: WAP to find the largest three elements in the array.
  */
 package Arrays;
+import java.util.Scanner;
 public class ques7 {
     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
 
-    }
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
 
-    public static class ques12 {
-        public static void main(String[] args) {
+        int array[] = new int[size];
 
+        System.out.print("Enter array elements : ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
         }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        int thridLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > largest) {
+                thridLargest = secondLargest;
+                secondLargest = largest;
+                largest = array[i];
+            } else if (array[i] > secondLargest) {
+                thridLargest = secondLargest;
+                secondLargest = array[i];
+            } else if (array[i] > thridLargest) {
+                thridLargest = array[i];
+            }
+        }
+
+        System.out.println("Largest three elements in the array are: " + largest + " " + secondLargest + " " + thridLargest);
     }
 }
